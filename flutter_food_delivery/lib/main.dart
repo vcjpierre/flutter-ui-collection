7ae31b8ab05f1b0d92fdbf8e2c10bdabd8481b0e
Jean Pierre <vcjpierre@gmail.com>
2019-10-05 16:55:13 -0400

    Initial commit 🎉🚀

---
+++ flutter_food_delivery/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/pages/StarterPage.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Roboto'),
+    home: StarterPage(),
+  )
+);
