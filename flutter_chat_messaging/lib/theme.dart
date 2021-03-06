567e4eb4ec9122389158b8f129245baf3aaf137b
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-06 02:26:27 -0400

    Add dark theme 🌑

---
+++ flutter_chat_messaging/lib/theme.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_chat_messaging/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData lightThemeData(BuildContext context) {
+  return ThemeData.light().copyWith(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: appBarTheme,
+    iconTheme: IconThemeData(color: kContentColorLightTheme),
+    textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)
+        .apply(bodyColor: kContentColorLightTheme),
+    colorScheme: ColorScheme.light(
+      primary: kPrimaryColor,
+      secondary: kSecondaryColor,
+      error: kErrorColor,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      selectedItemColor: kContentColorLightTheme.withOpacity(0.7),
+      unselectedItemColor: kContentColorLightTheme.withOpacity(0.32),
+      selectedIconTheme: IconThemeData(color: kPrimaryColor),
+      showUnselectedLabels: true,
+    ),
+  );
+}
+
+ThemeData darkThemeData(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kContentColorLightTheme,
+    appBarTheme: appBarTheme,
+    iconTheme: IconThemeData(color: kContentColorDarkTheme),
+    textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)
+        .apply(bodyColor: kContentColorDarkTheme),
+    colorScheme: ColorScheme.dark().copyWith(
+      primary: kPrimaryColor,
+      secondary: kSecondaryColor,
+      error: kErrorColor,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: kContentColorLightTheme,
+      selectedItemColor: Colors.white70,
+      unselectedItemColor: kContentColorDarkTheme.withOpacity(0.32),
+      selectedIconTheme: IconThemeData(color: kPrimaryColor),
+      showUnselectedLabels: true,
+    ),
+  );
+}
+
+final appBarTheme = AppBarTheme(centerTitle: false, elevation: 0);
